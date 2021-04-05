ff932813f0843bfe8f09ddcd1f02d6a31bfd4226
Jason Widdowson <jason.widdowson@employment.gov.au>
2021-04-05 13:33:55 +1000

    Added material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { ResultListComponent } from './result-list/result-list.component';
+import { ResultDetailComponent } from './result-detail/result-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    ResultListComponent,
+    ResultDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/result-detail/result-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-result-detail',
+  templateUrl: './result-detail.component.html',
+  styleUrls: ['./result-detail.component.scss']
+})
+export class ResultDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
